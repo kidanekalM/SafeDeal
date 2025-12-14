d191d4d0536957441852aaccc66ceda16d772f59
kidanekal <akidanekal@gmail.com>
2025-12-14 09:42:10 +0300

    upd cors

--- Backend/Api-gateway/internal/middleware/cors.go
+++ Backend/Api-gateway/internal/middleware/cors.go
@@ -17,7 +17,7 @@ func CORSMiddleware() fiber.Handler {
 
         c.Set("Vary", "Origin")
         c.Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH")
-        c.Set("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, Authorization, X-User-ID")
+        c.Set("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, Authorization, X-User-ID, ngrok-skip-browser-warning")
         c.Set("Access-Control-Allow-Credentials", "true")
 
         if c.Method() == "OPTIONS" {
